f8807411c21638384631b2af78fd34483f99bd5b
Heissendo <heissendo88@gmail.com>
2024-11-02 21:19:42 +0200

    separated database related code from app.js

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const connectToDB = require('./db');
 
 const app = express();
-const { MongoClient } = require("mongodb");
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
 
-//.env setup
-require('dotenv').config()
-const mongodbUri = process.env.MONGODB_URI;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,44 +40,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-// Define the listitem schema
-const shoppingListItemSchema = new Schema({
-    name: String,
-    amount: Number,
-});
-
-// Define the listitem model
-const SLI = mongoose.model('Shoppinglist item', shoppingListItemSchema);
-
-// Start the application
-main().catch(err => console.log(err));
-
-async function main() {
-
-    // Wait for connection, use database test
-    await mongoose.connect(mongodbUri);
-
-    // Hydrate a user model with instance values
-    const test = new SLI({
-        name: "Kurkku",
-        amount: 5000
-    });
-
-    const test2 = new SLI({
-        name: "Porkkana",
-        amount: 5000
-    });
-
-    // Persist the model instance in the database
-    test.save();
-    test2.save();
-    console.log("Saved");
-
-    // Fetch and print all model instances
-    const listitems = await SLI.find();
-    console.log(listitems);
-}
+connectToDB();
 
 
 module.exports = app;
---
+++ db.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+//.env setup
+require('dotenv').config()
+const mongodbUri = process.env.MONGODB_URI;
+
+// Define the listitem schema
+const shoppingListItemSchema = new Schema({
+    name: String,
+    amount: Number,
+});
+
+// Define the listitem model
+const SLI = mongoose.model('Shoppinglist item', shoppingListItemSchema);
+
+async function connectToDB() {
+
+    // Wait for connection, use database test
+    await mongoose.connect(mongodbUri);
+
+    // Hydrate a user model with instance values
+    const test = new SLI({
+        name: "Kurkku",
+        amount: 5000
+    });
+
+    const test2 = new SLI({
+        name: "Porkkana",
+        amount: 5000
+    });
+
+    // Persist the model instance in the database
+    test.save();
+    test2.save();
+    console.log("Saved");
+
+    // Fetch and print all model instances
+    const listitems = await SLI.find();
+    console.log(listitems);
+}
+
+module.exports = connectToDB;
