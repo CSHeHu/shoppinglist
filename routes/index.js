5f53e9bbf95e28b3d3e9c056e06c7eff29b6ad28
heissendo <heissendo88@gmail.com>
2024-12-02 21:55:04 +0200

    write adding db items into index.ejs. write css reset

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,15 @@ router.get('/', async function(req, res, next) {
   
     const response = await fetch('http://localhost:3000/data');  
     const data = await response.json();  
-
+/*
     // Log the fetched data
     console.log("Fetched data from /data:", data);
-
+*/
     
-    res.render('index', { title: 'Shopping List' });
+    res.render('index', 
+      { title: 'Shopping List', 
+        data : data
+      });
 
   } catch (err) {
     next(err);  
