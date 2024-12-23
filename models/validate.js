95d3a0f2f3ab91fc191e4daada0fcae24cb4a9b7
heissendo <heissendo88@gmail.com>
2024-12-23 18:42:00 +0200

    fix typos

--- models/validate.js
+++ models/validate.js
@@ -2,8 +2,8 @@ const { body, validationResult } = require('express-validator');
 
 // Validation rules
 const validateItem = [
-    body('name').isString().isLength({ min: 1, max: 100 })
-    body('amount').isInt({ min: 1, max: 1000 })
+    body('name').isString().isLength({ min: 1, max: 100 }),
+    body('amount').isInt({ min: 1, max: 1000 }),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
