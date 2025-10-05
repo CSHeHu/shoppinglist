8569b1e0b77883eb78c1edf669941756fcf3aaa9
Heikki Huttu <heissendo88@gmail.com>
2025-10-05 14:09:49 +0300

    Comment out remaining force-imports; will add real tests for these files later

--- tests/force-imports.test.js
+++ tests/force-imports.test.js
@@ -1,12 +1,8 @@
 // This file is used to force c8 to include all source files in coverage reports.
-import '../controllers/dashboardController.js';
-import '../controllers/itemController.js';
-import '../controllers/recipeController.js';
-import '../middleware/errorHandler.js';
-import '../middleware/validate.js';
-import '../models/itemModel.js';
-import '../routes/dashboardRoutes.js';
-import '../routes/itemRoutes.js';
-import '../routes/recipeRoutes.js';
-import '../routes/users.js';
-import '../services/recipeService.js';
+// import '../middleware/errorHandler.js';
+// import '../middleware/validate.js';
+// import '../models/itemModel.js';
+// import '../routes/dashboardRoutes.js';
+// import '../routes/itemRoutes.js';
+// import '../routes/recipeRoutes.js';
+// import '../routes/users.js';
