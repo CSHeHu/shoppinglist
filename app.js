cdde4bc77af856449dcc3082edc2843c1637296a
Heikki Huttu <heissendo88@gmail.com>
2025-12-22 14:38:18 +0200

    add env to app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ import errorHandler from './middleware/errorHandler.js';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 import { client as mongoClient } from './config/db.js';
+import 'dotenv/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
