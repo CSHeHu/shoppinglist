5eb5f72bd84b1b8df2ffbd6d9f9d020ab4c4967e
Heikki Huttu <heissendo88@gmail.com>
2025-12-31 16:04:36 +0200

    set samesite strict

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    maxAge: 24 * 60 * 60 * 1000 // 1 day
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    sameSite: 'strict'
   }
 }));
 
